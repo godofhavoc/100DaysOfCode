e8b5ffb4ed6cd35155d8a946bd8857954d6d6257
Akshay Surya <akshay163@gmail.com>
2018-01-05 00:09:38 +0530

    merge sort partially done

--- sorts.py
+++ sorts.py
@@ -1,3 +1,5 @@
+import math
+
 def insertion_sort(a):
     for j, value in enumerate(a):
         if j == 0:
@@ -9,3 +11,27 @@ def insertion_sort(a):
         a[i + 1] = value
 
     return a
+
+def merge(a, p, q, r):
+    l1 = a[p:q]
+    l1.append(math.inf)
+    l2 = a[q:r]
+    l2.append(math.inf)
+    i = 0
+    j = 0
+    for k in range(r):
+        if l1[i] > l2[j]:
+            a[k] = l1[i]
+            i += 1
+        else:
+            a[k] = l2[j]
+            j += 1
+
+def merge_sort(a, p, r):
+    if p < r:
+        q = math.floor((p+r)/2)
+        merge_sort(a, p, q)
+        merge_sort(a, q+1, r)
+        merge(a, p, q, r)
+    else:
+        return a
