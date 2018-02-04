97d7a97236471f7340983e998f26724c4aca68db
Akshay Surya <akshay163@gmail.com>
2018-02-04 13:10:45 +0530

    red black tree insertion

--- python/red_black_tree.py
+++ python/red_black_tree.py
@@ -39,3 +39,56 @@ def right_rotate(T, x):
         x.parent.right = y
     y.right = x
     x.parent = y
+
+def rb_insert(T, z):
+    y = None
+    x = T.root
+    while not x == None:
+        y = x
+        if z.data < x.data:
+            x = x.left
+        else:
+            x = x.right
+    z.parent = y
+    if y == None:
+        T.root = z
+    elif z.data < y.data:
+        y.left = z
+    else:
+        y.right = z
+    z.left = None
+    z.right = None
+    z.color = 'RED'
+    rb_insert_fixup(T, z)
+
+def rb_insert_fixup(T, z):
+    while z.parent.color == 'RED':
+        if z.parent == z.parent.parent.left:
+            y = z.parent.parent.right
+            if y.color == 'RED':
+                z.parent.color = 'BLACK'
+                y.color = 'BLACK'
+                z.parent.parent.color = 'RED'
+                z = z.parent.parent
+            else:
+                if z == z.parent.right:
+                    z = z.parent
+                    left_rotate(T, z)
+                z.parent.color = 'BLACK'
+                z.parent.parent.color = 'RED'
+                right_rotate(T, z.parent.parent)
+        else:
+            y = z.parent.parent.left
+            if y.color == 'BLACK':
+                z.parent.color = 'BLACK'
+                y.color = 'BLACK'
+                z.parent.parent.color = 'RED'
+                z = z.parent.parent
+            else:
+                if z == z.parent.left:
+                    z = z.parent
+                    right_rotate(T, z)
+                z.parent.color = 'BLACK'
+                z.parent.parent.color = 'RED'
+                left_rotate(T, z.parent.parent)
+    T.root.color = 'BLACK'
