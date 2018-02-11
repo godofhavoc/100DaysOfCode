4800f285fb08fa53049dc0fd0c473c578d771f72
Akshay Surya <akshay163@gmail.com>
2018-02-11 16:22:59 +0530

    optimal bst

--- python/dynamic_programming.py
+++ python/dynamic_programming.py
@@ -147,3 +147,22 @@ def print_lcs(b, x, i, j):
         print_lcs(b, x, i - 1, j)
     else:
         print_lcs(b, x, i, j - 1)
+
+def optimal_bst(p, q, n):
+    e = [[None for _ in range(n + 1)] for _ in range(1, n + 2)]
+    w = [[None for _ in range(n + 1)] for _ in range(1, n + 2)]
+    root = [[None for _ in range(1, n + 1)] for _ in range(1, n + 1)]
+    for i in range(1, n + 2):
+        e[i, i - 1] = q[i - 1]
+        w[i, i - 1] = q[i - 1]
+    for l in range(1, n + 1):
+        for i in range(1, n - l + 2):
+            j = i + l - 1
+            e[i, j] = math.inf
+            w[i, j] = w[i, j - 1] + p[j] + q[j]
+            for r in range(i, j + 1):
+                t = e[i, r - 1] + e[r + 1, j] + w[i, j]
+                if t < e[i, j]:
+                    e[i, j] = t
+                    root[i, j] = r
+    return e, root
