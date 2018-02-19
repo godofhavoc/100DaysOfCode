74a2de1a27a0849b293e802ca8dc87301ba409a9
Akshay Surya <akshay163@gmail.com>
2018-02-19 20:22:44 +0530

    small additions

--- python/van_emde_boas.py
+++ python/van_emde_boas.py
@@ -1,3 +1,16 @@
+import math
+
+u = 256
+
+def high(x):
+    return math.floor(x/math.sqrt(u))
+
+def low(x):
+    return x % math.sqrt(x)
+
+def index(x, y):
+    return x * math.sqrt(x) + y
+
 def proto_veb_member(V, x):
     if V.u == 2:
         return V.A[x]
