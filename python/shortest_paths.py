016da8916a84ea8eec15a634b6b34a7a5a154cdb
Akshay Surya <akshay163@gmail.com>
2018-02-24 08:42:27 +0530

    shortes path matrix multiplication

--- python/shortest_paths.py
+++ python/shortest_paths.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 def initialize_single_source(G, s):
     for v in G.V:
@@ -30,3 +31,20 @@ def dijkstra(G, w, s):
         S += u
         for v in G.Adj[u]:
             relax(u, v, w)
+
+def extend_shortest_paths(L, W):
+    n = len(L)
+    l1 = np.zeros(shape=(n, n))
+    for i in range(n):
+        for j in range(n):
+            l1[i][j] = math.inf
+            for k in range(n):
+                l1[i][j] = min(l[i][j], L[i][k] + W[k][j])
+    return l1
+
+def slow_all_pairs_shortest_paths(W):
+    n = W.rows
+    L = W
+    for m in range(1, n - 1):
+        lm = extend_shortest_paths(L, W)
+    return L
